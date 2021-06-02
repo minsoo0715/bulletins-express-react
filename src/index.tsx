75e54fe1f5a0b530b6c65f0a6337acc084c49dcd
WhiteB <whiteblock0715@gmail.com>
2021-06-02 16:41:14 +0900

    server settings

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+import './asset/style/style.css'
+ReactDOM.render(
+    <App/>,
+    document.getElementById("App")
+);
