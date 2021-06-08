cc43fb580fd4649e9a845a9317e30b6df6fa3124
WhiteB <whiteblock0715@gmail.com>
2021-06-09 08:39:56 +0900

    add states routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,79 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Boxlist from "./components/Boxlist"
 import Btn from "./components/Btn";
 import Inputbox from "./components/Inputbox";
 import Text from './asset/file/text.txt'
 import Json from './asset/file/object.json'
 import reactLogo from './asset/img/reactlogo.png'
+import InputPage from './Pages/InputPage'
+import IndexPage from './Pages/IndexPage'
 
 
 
 export function App() {
-    const [bulletin_list, Setbulletin] = useState([])
-    const [isChanged, SetisChanged] = useState(true)
-    const [mode, Setmode] = useState(0) 
+    const [bulletin_list, Setbulletin] = useState<Array<box>>([])
+    const [isChanged, SetisChanged] = useState<boolean>(true)
+    const [mode, Setmode] = useState<number>(0)
 
-    let loadData = async () => {
-        let response = await fetch('http://localhost:8000/bulletins');
-        let _data = await response.json()
-        Setbulletin(_data.data)
-        SetisChanged(false)
-        return
-    }
-
-    useEffect( () => {
-        if(isChanged) {
+    useEffect(() => {
+        if (isChanged || mode == 0) {
             loadData()
         }
         SetisChanged(false)
     })
 
-    
+    const loadData = async () => {
+        console.log('GET')
+        const response = await fetch('http://localhost:8000/bulletins');
+        const _data = await response.json()
+        Setbulletin(_data.data)
+        return
+    }
 
     function GetMain() {
 
-        if(mode == 1) {
-            return (
-                
-                <Inputbox onSubmit={function(_title:string, _content:string) {
-                    let newBox:box = {title: _title, content: _content}
-                    let newList:Array<box> = Array.from(bulletin_list);
-                    console.log(newBox, JSON.stringify(newBox))
+
+        switch (mode) {
+            case 0:
+                const clickAction: Function = () => Setmode(1)
+                return (
+                    <IndexPage list={bulletin_list} event={clickAction}></IndexPage>
+                )
+            case 1:
+                const submitAction: Function = (_title: string, _content: string) => {
+                    const newBox: box = { title: _title, content: _content, id: bulletin_list.length }
+                    let newList: Array<box> = Array.from(bulletin_list);
                     fetch('http://localhost:8000/bulletins', {
-                    method: 'POST',
-                    body: JSON.stringify(newBox),
-                    mode: 'cors',
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-        
+                        method: 'POST',
+                        body: JSON.stringify(newBox),
+                        mode: 'cors',
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+
                     })
+
                     newList.push(newBox)
                     Setbulletin(newList)
                     Setmode(0)
-                    
-                    
-                }}
-                 ></Inputbox>
-    
-            )
-        }else if(mode == 0) {
-            return (
-            <Btn text="추가하기" event={
-                () => {
-                    Setmode(1)
                 }
-            }></Btn>
-            )
+
+                return (<InputPage onSubmit={submitAction}></InputPage>)
+            case 2:
+
+
+
+                break;
         }
     }
 
-   
-    return(
-        <div>
-            <Boxlist list={bulletin_list}></Boxlist>
 
+
+
+
+
+    return (
+        <div id="frame">
             {
                 GetMain()
             }
@@ -84,5 +85,7 @@ export function App() {
     )
 
 }
+
+
 
 
