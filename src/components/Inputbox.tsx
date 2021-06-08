d6b34198db5a2290bb1321ff6fc34780e6a596ad
WhiteB <whiteblock0715@gmail.com>
2021-06-09 08:38:23 +0900

    InputBox Style 수정

--- src/components/Inputbox.tsx
+++ src/components/Inputbox.tsx
@@ -9,9 +9,9 @@ function Inputbox(props : inputbox_props) {
                 
                 e.target.reset()
             }}>
-                <p> <input type="text" name="title" placeholder="제목"></input></p>
-                <p> <input type="textarea" name="content" placeholder="내용"></input></p>
-                <p> <input type="submit" value="완료"></input></p>
+                <p> <input type="text" name="title" id="title" placeholder="제목"></input></p>
+                <p> <textarea name="content" placeholder="내용" id="content" rows={50} cols={200}></textarea></p>
+                <p> <input type="submit" value="글 쓰기" id="bulletin_submit"></input></p>
             </form>
         </div>
     )
