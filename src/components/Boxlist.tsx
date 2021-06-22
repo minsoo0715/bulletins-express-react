f58f2f9b290fd2db1762be75381df98521155fa9
WhiteB <whiteblock0715@gmail.com>
2021-06-22 15:01:35 +0900

    add delete feature

--- src/components/Boxlist.tsx
+++ src/components/Boxlist.tsx
@@ -11,12 +11,28 @@ function Boxlist(props:boxlist) {
     let result: Array<JSX.Element> = []
 
     for(let b of props.list) {
-        result.push( <Smallbox key={b.id} title={b.title} content={b.content} > </Smallbox> );
+        result.push( <Smallbox key={b.id} delete={
+        async () => {
+             await fetch('http://whiteb.p-e.kr/bulletins', {
+                        method: 'DELETE',
+                        body: JSON.stringify({
+                            id: b.id
+                        }),
+                        mode: 'cors',
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+
+                    })
+                await props.refresh()
+        }} title={b.title} content={b.content} > </Smallbox> );
+        
+        
     }
 
 
     return (
-        <div>
+        <div className="test">
             {result}
         </div>    
     );
