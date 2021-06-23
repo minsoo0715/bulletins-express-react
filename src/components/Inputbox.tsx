e617cb1dee944fdd3434a679b59ab9ba20040c52
WhiteB <whiteblock0715@gmail.com>
2021-06-23 18:18:38 +0900

    p tag to div tag

--- src/components/Inputbox.tsx
+++ src/components/Inputbox.tsx
@@ -17,9 +17,9 @@ function Inputbox(props : inputbox_props) {
                 
                 e.target.reset()
             }}>
-                <p> <input type="text" name="title" id="title" placeholder="제목"></input></p>
-                <p> <textarea name="content" placeholder="내용" id="content" rows={50} cols={200}></textarea></p>
-                <p> <input type="submit" value="글 쓰기" id="bulletin_submit"></input></p>
+                <div> <input type="text" name="title" id="title" placeholder="제목"></input></div>
+                <div id="content"> <textarea name="content" placeholder="내용" id="content" rows={20}cols={200}></textarea></div>
+                <div> <input type="submit" value="글 쓰기" id="bulletin_submit"></input></div>
             </form>
         </div>
     )
